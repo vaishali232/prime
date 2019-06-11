f11fede234586a5f5430eb81dcc216518517b07f
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 18:56:31 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,flag=0;
+    scanf("%d",&n);
+    for(int i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+  
+}
